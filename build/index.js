1c677b299cd870dbd5626e7b39c027770b6098ad
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-02 13:45:55 +0100

    created csv file reader and imort

--- build/index.js
+++ build/index.js
@@ -1,32 +1,28 @@
-import { CsvFileReader } from './CsvFileReader';
-
-const reader = new CsvFileReader('football.csv');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
 reader.read();
-
 console.log(reader.data);
-
 //enum --> enumeration
-
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
-
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
 // what is a enu? they follow near-identical sntax rules as nomral objects (except for the = instead of doppelpunkt)
 // creates an object with the same keys and values when converted from TS to JS. so if converted to JS, it turns into an object.
 // primary goal is to signal to other engineers that these are all closely related values, there aren't big advantages to using enums when it comes to using TS compiler, it's mainly for signaling.
-/* enums are used whenever we have a small fixed set of values that are all closely related and known at compile time, 
+/* enums are used whenever we have a small fixed set of values that are all closely related and known at compile time,
 so we shouldn't/cannot add new values to the enum while executing the code. They should be pre-defined, before the code runs */
-
 let manUnitedWins = 0;
-
 for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
 }
-
 console.log(`ManU won ${manUnitedWins} times, that's pretty fucking bad lel`);
