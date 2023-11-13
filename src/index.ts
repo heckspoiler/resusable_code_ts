ace1d5b08bd85d65c3d231644f567ce206fb8421
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-11-13 23:53:51 +0100

    changes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from './inheritance/CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
